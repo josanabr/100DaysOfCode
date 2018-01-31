d3b06cf3063e2b915866366a0507c86910b2c47b
John Sanabria <john.sanabria@gmail.com>
2018-01-30 21:46:00 -0500

    Replanteando arquitectura

--- docker/app_psql.py
+++ docker/app_psql.py
@@ -1,15 +1,19 @@
 import os
 
 from flask import Flask
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+db = SQLAlchemy()
 
 @app.route('/')
 def hello():
-    return 'Hello Flask from alpine-linux!'
+    return 'Hello world!'
 
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
